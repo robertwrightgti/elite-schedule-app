c181514f4100d1a9e1a5ffa668ee316ff5279774
Rob Wright <rob.wright@groupgti.com>
2017-09-26 16:53:03 +0100

    more changes

--- src/pages/pages.ts
+++ src/pages/pages.ts
@@ -1,5 +1,5 @@
 export * from './my-teams/my-teams.page';
 export * from './tournaments/tournaments.page';
+export * from './teams/teams.page';
 export * from './team-detail/team-detail.page';
 export * from './game/game.page';
-export * from './teams/teams.page';
