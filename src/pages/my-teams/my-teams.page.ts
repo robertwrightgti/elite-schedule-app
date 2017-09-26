ca694faebdb303bc3dfc20207494e38ff5adf659
Rob Wright <rob.wright@groupgti.com>
2017-09-26 15:59:56 +0100

    adding a really simple navigation change (from my-teams page to tournaments page)

--- src/pages/my-teams/my-teams.page.ts
+++ src/pages/my-teams/my-teams.page.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+import { TournamentsPage } from '../pages';
 
 @Component({
     templateUrl: 'my-teams.page.html'
 })
 export class MyTeamsPage {
-    constructor(){}
+    constructor(private nav: NavController){}
+
+    goToTournaments() {
+        this.nav.push(TournamentsPage );
+    }
 }
