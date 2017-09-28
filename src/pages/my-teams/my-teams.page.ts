96a98b9d7626a9d41d0c7cb304cee599364bd823
Rob Wright <rob.wright@groupgti.com>
2017-09-28 12:37:41 +0100

    fake faves on home page (ion-list)

--- src/pages/my-teams/my-teams.page.ts
+++ src/pages/my-teams/my-teams.page.ts
@@ -1,15 +1,44 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { LoadingController, NavController } from 'ionic-angular';
 
-import { TournamentsPage } from '../pages';
+import { TeamHomePage, TournamentsPage } from '../pages';
+import { EliteApi } from '../../shared/shared';
 
 @Component({
     templateUrl: 'my-teams.page.html'
 })
 export class MyTeamsPage {
-    constructor(private nav: NavController){}
+
+    favorites = [
+        {
+            team: { id: 6182, name: 'HC Elite 7th', coach: 'Michelotti' },
+            tournamentId: '89e13aa2-ba6d-4f55-9cc2-61eba6172c63',
+            tournamentName: 'March Madness Tournament'
+        },
+        {
+            team: { id: 805, name: 'HC Elite', coach: 'Michelotti' },
+            tournamentId: '98c6857e-b0d1-4295-b89e-2d95a45437f2',
+            tournamentName: 'Holiday Hoops Challenge'
+        }
+    ];
+
+    constructor(
+        private nav: NavController,
+        private loadingController: LoadingController,
+        private eliteApi: EliteApi
+    ){}
 
     goToTournaments() {
         this.nav.push(TournamentsPage );
+    }
+
+    favoriteTapped($event, favorite) {
+        let loader = this.loadingController.create({
+            content: 'Getting data...',
+            dismissOnPageChange: true
+        });
+        loader.present();
+        this.eliteApi.getTournamentData(favorite.tournamentId)
+            .subscribe(t => this.nav.push(TeamHomePage, favorite.team));
     }
 }
